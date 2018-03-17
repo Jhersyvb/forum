2372c59ecc9c4b74520163ffcd1519ff496b1aee
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2018-03-17 11:10:39 -0500

    Episode 9. A Thread should be assigned a Channel

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,9 @@ $factory->define(App\Thread::class, function ($faker) {
         'user_id' => function () {
             return factory('App\User')->create()->id;
         },
+        'channel_id' => function () {
+            return factory('App\Channel')->create()->id;
+        },
         'title' => $faker->sentence,
         'body' => $faker->paragraph,
     ];
@@ -44,3 +47,11 @@ $factory->define(App\Reply::class, function ($faker) {
     ];
 });
 
+$factory->define(App\Channel::class, function ($faker) {
+    $name = $faker->word;
+
+    return [
+        'name' => $name,
+        'slug' => $name,
+    ];
+});
