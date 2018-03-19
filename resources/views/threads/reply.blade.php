95ee5e2b371353072f0ff675ca83e589b57d0eaa
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2018-03-19 11:33:10 -0500

    Episode 19. The Favorite Button

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,21 @@
 <div class="card my-3">
     <div class="card-header">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a> said {{ $reply->created_at->diffForHumans() }}...
+        <div class="row">
+            <div class="col">
+                <a href="#">
+                    {{ $reply->owner->name }}
+                </a> said {{ $reply->created_at->diffForHumans() }}...
+            </div>
+
+            <div class="col-auto">
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    {{ csrf_field() }}
+                    <button type="submit" class="btn btn-default" {{ $reply->isFavorited() ? 'disabled' : '' }}>
+                        {{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count()) }}
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
 
     <div class="card-body">
