31c928d85aabb139d738c67213143c2dd5b3bfc7
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2018-03-22 12:55:08 -0500

    Episode 31. Authorized Users can delete Replies

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Thread;
-use Illuminate\Http\Request;
+use App\{Thread, Reply};
 
 class RepliesController extends Controller
 {
@@ -22,5 +21,14 @@ class RepliesController extends Controller
         ]);
 
         return back()->with('flash', 'Your reply has been left.');;
+    }
+
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('update', $reply);
+
+        $reply->delete();
+
+        return back();
     }
 }
--- resources/views/profiles/activities/created_favorite.blade.php
+++ resources/views/profiles/activities/created_favorite.blade.php
@@ -1,8 +1,8 @@
 @component('profiles.activities.activity')
     @slot('heading')
         <a href="{{ $activity->subject->favorited->path() }}">
-            {{ $profileUser->name }}
-        </a> favorited a reply.
+            {{ $profileUser->name }} favorited a reply.
+        </a>
     @endslot
 
     @slot('body')
