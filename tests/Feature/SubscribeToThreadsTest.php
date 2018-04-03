a59269086126f5ef527cd0aa03d212685d54c0ab
Jhersy Valer Bejarano <Jhersyvb@gmail.com>
2018-04-03 10:35:31 -0500

    Episode 42. Thread Subscriptions: part 3

--- tests/Feature/SubscribeToThreadsTest.php
+++ tests/Feature/SubscribeToThreadsTest.php
@@ -14,15 +14,34 @@ class SubscribeToThreadsTest extends TestCase
     {
         $this->signIn();
 
+        // Given we have a thread...
         $thread = create('App\Thread');
 
+        // And the user subscribes to the thread...
         $this->post($thread->path() . '/subscriptions');
 
+        // Then, each time a new reply is left...
         $thread->addReply([
             'user_id' => auth()->id(),
             'body' => 'Some reply here'
         ]);
 
-        $this->assertCount(1, auth()->user()->notifications);
+        // A notification should be prepared for the user.
+        // TODO:
+        // $this->assertCount(1, auth()->user()->notifications);
+    }
+
+    /** @test */
+    function a_user_can_unsubscribe_from_threads()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $thread->subscribe();
+
+        $this->delete($thread->path() . '/subscriptions');
+
+        $this->assertCount(0, $thread->subscriptions);
     }
 }
